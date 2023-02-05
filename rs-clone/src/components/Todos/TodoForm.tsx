a18171eec5b6f0c8f59ef222200fb3aa6a2c50a9
George Koloidi <Battleaxe@bk.ru>
2023-02-05 18:56:19 +0100

    feat: add icon for TodoForm

--- rs-clone/src/components/Todos/TodoForm.tsx
+++ rs-clone/src/components/Todos/TodoForm.tsx
@@ -1,21 +1,27 @@
 import {useState} from 'react';
 import styles from './styles/TodoForm.module.scss';
+import {FiPlus} from 'react-icons/fi';
+
+type TodoEvent = React.FormEvent<HTMLFormElement> | React.MouseEvent<SVGElement>
 
 const TodoForm = () => {
   const [text, setText] = useState('');
-  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmitHandler = (e: TodoEvent ) => {
     e.preventDefault();
     setText('');
   };
   return (
     <form className={styles.form} onSubmit={onSubmitHandler}>
-      <input
-        className={styles.input}
-        type="text"
-        placeholder='Adding task in "Tasks". Press Enter for save'
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
+      <div className={styles.input_wrapper}>
+        <FiPlus className={styles.input_add} onClick = {onSubmitHandler}/>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder='Adding task in "Tasks". Press Enter for save'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
     </form>
   );
 };
