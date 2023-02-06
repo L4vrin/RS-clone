9a88c8e05900a30decbac77337deb0ecc68d6f77
George Koloidi <Battleaxe@bk.ru>
2023-02-06 02:06:09 +0100

    refactor:  delete spaces, add semelicons. Change color on incons

--- rs-clone/src/components/Todos/TodoCompletedList.tsx
+++ rs-clone/src/components/Todos/TodoCompletedList.tsx
@@ -1,12 +1,12 @@
 import {ITodo} from './interfaces/todiInterfaces';
 import Todo from './Todo';
-import styles from './styles/TodoCompletedList.module.scss'
+import styles from './styles/TodoCompletedList.module.scss';
 
 const TodoCompletedList = ({todos, deleteTodo, toggleTodo}: any) => {
-  const filterCompletedTodos = (todos: Array<ITodo> ) => {
-    return todos.filter((todo: ITodo) => todo.isCompleted)
-  }
-  const completedTodos = filterCompletedTodos(todos)
+  const filterCompletedTodos = (todos: Array<ITodo>) => {
+    return todos.filter((todo: ITodo) => todo.isCompleted);
+  };
+  const completedTodos = filterCompletedTodos(todos);
   return (
     <>
       <div className={styles.todoCompletedList}>
