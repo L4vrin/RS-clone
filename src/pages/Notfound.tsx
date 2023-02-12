1d74bb386072feac08bf1da67f65a3f6c3225f12
George Koloidi <Battleaxe@bk.ru>
2023-02-12 22:37:09 +0100

    fix: styles in 404 page

--- src/pages/Notfound.tsx
+++ src/pages/Notfound.tsx
@@ -6,8 +6,8 @@ const Notfound = () => {
       <figure className={styles.figure}>
         <img className={styles.img} src="/images/img404.webp" alt="error-404" />
         <figcaption className={styles.figcaption}>
-          <h2>Error 404 - page not found</h2>
-          <h2>Please, keep calm and return to previous page</h2>
+          <h2 className={styles.title}>Error 404 - page not found</h2>
+          <h2 className={styles.text}>Please, keep calm and return to previous page</h2>
         </figcaption>
       </figure>
     </div>
