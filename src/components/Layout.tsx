b9a14c215c7e7b7f3484427116c3afe8b6c9e7b5
George Koloidi <Battleaxe@bk.ru>
2023-02-13 16:47:32 +0100

    fix: div

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import Timer from './Timer';
 import TimerSettingsWidget from './TimerSettingsWidget/TimerSettingsWidget';
-import styles from './Layout.module.scss'
+import styles from './Layout.module.scss';
 
 const Layout = () => {
   return (
     <>
-      <Header/>
-      <div className={styles.layoutContainer} />
-      <Outlet />
+      <Header />
+      <div className={styles.layoutContainer}>
+        <Outlet />
+      </div>
       <Timer />
       <TimerSettingsWidget />
       <Footer />
     </>
-  )
-}
+  );
+};
 
 export default Layout;
