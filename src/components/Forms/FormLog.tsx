8e94046ab0d565a6c87757c189a4b3bc3d5a0b29
George Koloidi <Battleaxe@bk.ru>
2023-02-14 12:52:49 +0100

    feat: display todo only for current logined user

--- src/components/Forms/FormLog.tsx
+++ src/components/Forms/FormLog.tsx
@@ -34,6 +34,7 @@ const FormLog = () => {
       const userData = await loginUser(data).unwrap();
       changeUserName(userData.fullName);
       localStorage.setItem('token', userData.token)
+      localStorage.setItem('email', userData.email)
       navigate('today')
     } catch (err) {
       setErrorLog(err);
