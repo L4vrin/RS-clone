7a054fa49242615467aa2f636494e746ef8f6587
George Koloidi <Battleaxe@bk.ru>
2023-02-17 11:54:20 +0100

    fix: navigation routes

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -1,7 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import {Outlet, useNavigate} from 'react-router-dom';
+import {useEffect} from 'react';
 import SideBar from '../components/SideBar';
 
 const TasksPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate('today');
+  }, []);
+
   return (
     <>
       <SideBar />
