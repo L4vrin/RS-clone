c64f867d025d0f41f6448fa72219dca33aed8f45
l4vrin <l4vrin@gmail.com>
2023-02-19 18:02:20 +0300

    fix: conflicts before force push

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
 import styles from './Layout.module.scss';
 
 const Layout = () => {
   return (
-    <div className={styles.layoutContainer}>
-      <Outlet />
-    </div>
+    <>
+      <Header />
+      <div className={styles.layoutContainer}>
+        <Outlet />
+      </div>
+      <Footer />
+    </>
   );
 };
 
