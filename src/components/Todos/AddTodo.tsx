0f43b0e8be953c3a55c83dffa990aa9516dd9834
George Koloidi <Battleaxe@bk.ru>
2023-02-19 21:31:53 +0100

    feat: add animation to Add todo btn

--- src/components/Todos/AddTodo.tsx
+++ src/components/Todos/AddTodo.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
 import { HiPlusCircle } from 'react-icons/hi';
@@ -9,25 +10,41 @@ const AddTodo = ({ deadline }: { deadline: string }) => {
   const [isAddTask, setIsAddTask] = useState(false);
   const { t } = useTranslation();
   return (
-    <div className={`container ${styles.container}`}>
-      {isCreateMode ? (
-        <EditPanel onClose={() => setIsCreateMode(false)} isAdd={isAddTask} deadline={deadline} />
-      ) : (
-        <button
-          type="button"
-          className={styles.addButton}
-          onClick={() => {
-            setIsCreateMode(true);
-            setIsAddTask(true);
-          }}
-        >
-          <span className={styles.buttonIcon}>
-            <HiPlusCircle />
-          </span>
-          <span className={styles.buttonText}>{t('AddTask')}</span>
-        </button>
-      )}
-    </div>
+    <AnimatePresence>
+      <motion.div className={`container ${styles.container}`}>
+        {isCreateMode ? (
+          <motion.div
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: 'auto', opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+          >
+            <EditPanel
+              onClose={() => setIsCreateMode(false)}
+              isAdd={isAddTask}
+              deadline={deadline}
+            />
+          </motion.div>
+        ) : (
+          <motion.button
+            initial={{ height: 20 }}
+            animate={{ height: 40 }}
+            exit={{ height: 20 }}
+            transition={{ duration: 0.1 }}
+            type="button"
+            className={styles.addButton}
+            onClick={() => {
+              setIsCreateMode(true);
+              setIsAddTask(true);
+            }}
+          >
+            <span className={styles.buttonIcon}>
+              <HiPlusCircle />
+            </span>
+            <span className={styles.buttonText}>{t('AddTask')}</span>
+          </motion.button>
+        )}
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
