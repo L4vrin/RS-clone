c6a444f2039a5b2f97b7ecab78b73089d8a69463
George Koloidi <Battleaxe@bk.ru>
2023-02-27 12:28:29 +0100

    fix: remove import

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
-import styles from './Layout.module.scss';
 
 const Layout = () => {
   return (
